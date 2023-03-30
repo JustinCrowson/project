51755f98ecd2b8bbe0f58ab01456c35ad4553e2c
xAsdfaf <georgejordanjr8@gmail.com>
2023-03-29 21:39:53 -0500

    fixed snake game styling, made it more inline with the rest of the game pages. Score counter once again works.

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    'nictactoe_db',
-    'root',
-    'D!v4St4rz@13',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
 
     {
       host: '127.0.0.1',
